bdfe87f7f61c8ae3110e07bccc555f691a7936b8
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-05 15:03:28 +0300

    Events losting done

--- client/src/app/events/events.component.ts
+++ client/src/app/events/events.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
 })
 export class EventsComponent implements OnInit {
+  events: any[] = [];
 
-  constructor() { }
+  constructor(private _eventService: EventsService) {}
 
   ngOnInit(): void {
+    this._eventService.getEvents().subscribe(
+      (res) => (this.events = res),
+      (err) => console.log(err)
+    );
   }
-
 }
--- client/src/app/special-events/special-events.component.ts
+++ client/src/app/special-events/special-events.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'app-special-events',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./special-events.component.css']
 })
 export class SpecialEventsComponent implements OnInit {
+  specialEvents: any[] = []
 
-  constructor() { }
+  constructor(private _eventService: EventsService,
+              private _router: Router) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    this._eventService.getSpecialEvents()
+      .subscribe(
+        res => this.specialEvents = res,
+        err => {
+          if( err instanceof HttpErrorResponse ) {
+            if (err.status === 401) {
+              this._router.navigate(['/login'])
+            }
+          }
+        }
+      )
   }
 
 }
