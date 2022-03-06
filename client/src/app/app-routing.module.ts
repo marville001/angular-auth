f85c828d7cee2b51a1c392be11435a2913a1920a
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-06 15:23:25 +0300

    Protect special route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsComponent } from './events/events.component';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
@@ -18,6 +19,7 @@ const routes: Routes = [
   {
     path: 'special',
     component: SpecialEventsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
